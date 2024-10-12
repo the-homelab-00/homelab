a936a7af58a88f554d68f43777a51ceacc0fe64f
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2024-10-12 15:57:55 +0100

    delete

--- test.sh
+++
@@ -1,3 +0,0 @@
-for resource in cephblockpools cephclusters cephfilesystems cephfilesystemsubvolumegroups cephobjectstores cephclients cephbucketnotifications cephbuckettopics cephfilesystemmirrors cephnfses cephobjectrealms cephobjectstoreusers cephobjectzonegroups cephobjectzones cephrbdmirrors; do
-    kubectl delete $resource --all -n rook-ceph --grace-period=0 --force
-done
